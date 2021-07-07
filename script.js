5cb30e57a45844d533f2ba62806a5fa688bdd6c5
Eliton Netto <netinho_123_anps@hotmail.com>
2021-07-06 21:23:13 -0300

    Update - JS : Funcion LimparDados + update HTML & CSS - Main NAV

--- script.js
+++ script.js
@@ -3,19 +3,43 @@ function dadosRecebidos(){
   var nomeMercadoria = document.getElementById("nomeMercadoria").value
   var Valor = document.getElementById("Valor").value
 
+  if(tipoTrans ==="Selecione"||nomeMercadoria==""||Valor==""){
+    alert("Preencha os Campos!")
+    location.reload();
+  }
+  else{
+       /* Inserir Linhas na Tabela*/
+    var tabela = document.getElementsByTagName("table")[0];
+    var novaLinha = tabela.insertRow(1);
+
+    var col1 = novaLinha.insertCell(0);
+    var col2 = novaLinha.insertCell(1);
+    var col3 = novaLinha.insertCell(2);
+
+    col1.innerHTML =  tipoTrans;
+    col2.innerHTML = nomeMercadoria;
+    col3.innerHTML = Valor;
+  }
+}
+
+function limparDados(){
+
+  localStorage.clear()
+  location.reload();
+
+}
+function salvarDados(){
+
+  var tipoTrans = document.getElementById("tipo-trans").value
+  var nomeMercadoria = document.getElementById("nomeMercadoria").value
+  var Valor = document.getElementById("Valor").value
+
   var tipo = localStorage.setItem('Tipo', tipoTrans)
   var nome = localStorage.setItem('Mercadoria', nomeMercadoria)
   var valor = localStorage.setItem('Valor', Valor)
 
-/* Inserir Linhas na Tabela*/
-  var tabela = document.getElementsByTagName("table")[0];
-  var novaLinha = tabela.insertRow(0);
+}
+function cadastro(){
 
-  var col1 = novaLinha.insertCell(0);
-  var col2 = novaLinha.insertCell(1);
-  var col3 = novaLinha.insertCell(2);
 
-  col1.innerHTML =  tipoTrans
-  col2.innerHTML = nomeMercadoria
-  col3.innerHTML = Valor
 }
