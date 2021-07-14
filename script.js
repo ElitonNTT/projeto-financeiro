cbd389b4559998846b612a523107ef169b17ff04
Eliton Netto <netinho_123_anps@hotmail.com>
2021-07-13 21:58:25 -0300

    Testando função para salvar tabela em localStorage

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 function dadosRecebidos(){
-  var tipoTrans = document.getElementById("tipo-trans").value
-  var nomeMercadoria = document.getElementById("nomeMercadoria").value
-  var Valor = document.getElementById("Valor").value
-  var auxiliar = document.getElementById("tipo-trans").value
 
-if(auxiliar == "Compra"){
-  auxiliar = "+"
-}
-if(auxiliar == "Venda"){
-  auxiliar = "-"
-}
+    var tipoTrans = document.getElementById("tipo-trans").value
+    var nomeMercadoria = document.getElementById("nomeMercadoria").value
+    var Valor = document.getElementById("Valor").value
+    var auxiliar = document.getElementById("tipo-trans").value
 
-  if(tipoTrans ==="Selecione"||nomeMercadoria==""||Valor==""){
-    alert("Preencha os Campos!")
-    location.reload();
-  }
-  else{
-       /* Inserir Linhas na Tabela*/
+   if(auxiliar == "Compra"){
+      auxiliar = "+"
+    }
+    else if(auxiliar == "Venda"){
+      auxiliar = "-"
+    }
+    if(tipoTrans ==="Selecione"||nomeMercadoria==""||Valor==""){
+      alert("Preencha os Campos!")
+      location.reload();
+    }else{
     var tabela = document.getElementsByTagName("table")[0];
-    var novaLinha = tabela.insertRow(1);
-
+    var novaLinha = tabela.insertRow(-1);
+  
     var col1 = novaLinha.insertCell(0);
     var col2 = novaLinha.insertCell(1);
     var col3 = novaLinha.insertCell(2);
-
+  
     col1.innerHTML = auxiliar
     col2.innerHTML =  nomeMercadoria;
-    col3.innerHTML = Valor;
-  }
+    col3.innerHTML = Valor
+    }
 }
 
 function limparDados(){
@@ -38,16 +36,32 @@ function limparDados(){
 }
 function salvarDados(){
 
-  var tipoTrans = document.getElementById("tipo-trans").value
-  var nomeMercadoria = document.getElementById("nomeMercadoria").value
-  var Valor = document.getElementById("Valor").value
+  let array = []
+  
+  let tipoStorage = document.querySelector("tipo-trans")
 
-  var tipo = localStorage.setItem('Tipo', tipoTrans)
-  var nome = localStorage.setItem('Mercadoria', nomeMercadoria)
-  var valor = localStorage.setItem('Valor', Valor)
+  let nomeStorage = document.querySelector("nomeMercadoria")
+ 
+  let valorStorage = document.querySelector("Valor")
+
+  array = {tipo: tipoStorage, nome:nomeStorage,valor: valorStorage}
+
+  localStorage.setItem("table", JSON.stringify(array))
 
 }
 function cadastro(){
+
+
+}
+
+function validacao(){
+
+
+}
+
+function calcularLucro(){
+
+
 
 
 }
